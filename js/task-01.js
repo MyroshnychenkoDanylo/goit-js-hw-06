9c617c8bab86cb1babbe9a6515972d2f2a043784
MyroshnychenkoDanylo <myroshnychenko0203@gmail.com>
2023-03-18 15:12:51 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const allItemREF = document.querySelectorAll(".item");
+const lengthOfAllItemREF = allItemREF.length;
+
+console.log(`Number of categories: ${lengthOfAllItemREF}`);
+
+const animalsREF = document.querySelectorAll(".item.h2");
+console.log(allItemREF[0]);
+// console.log(animalsREF);
