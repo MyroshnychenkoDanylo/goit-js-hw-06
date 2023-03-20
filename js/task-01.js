1d7a9ff35fc3ca32c8ae7463343ac73c9e8072f6
MyroshnychenkoDanylo <myroshnychenko0203@gmail.com>
2023-03-20 09:56:49 +0200

    ex1

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,14 @@ const lengthOfAllItemREF = allItemREF.length;
 
 console.log(`Number of categories: ${lengthOfAllItemREF}`);
 
-const animalsREF = document.querySelectorAll(".item.h2");
-console.log(allItemREF[0]);
-// console.log(animalsREF);
+// const animalsREF = document.querySelectorAll(".item.h2");
+// console.log(allItemREF[0]);
+// // console.log(animalsREF);
+
+const itemREF = document.querySelector("ul#categories .item");
+console.log(itemREF);
+
+allItemREF.forEach((el) =>
+  console.log(`Category: ${el.firstElementChild.textContent}
+  Elements: ${el.lastElementChild.childElementCount}`)
+);
